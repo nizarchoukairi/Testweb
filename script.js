8dd50ae1903ac2aad0bdc001ad5149c309f7526b
nizarchoukairi <120064662+nizarchoukairi@users.noreply.github.com>
2023-07-09 16:01:36 +0100

    Web
    Web

---
+++ script.js
@@ -0,0 +1,7 @@
+function fnc() {
+	let n1 = document.getElementById('n1').value;
+	let n2 = document.getElementById('n2').value;
+	let h2 = document.getElementById("h2");
+	h2.innerHTML = n1*n2;
+	h2.style.left = "185px";
+}
